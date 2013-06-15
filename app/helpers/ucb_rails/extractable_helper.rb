70a9b561293234409a3e380f9688b6f4c5591ff6
Steve Downey <steve.downtown@gmail.com>
2013-06-14 17:51:57 -0700

    cleaned up lsp typeahead issues

--- app/helpers/ucb_rails/extractable_helper.rb
+++ app/helpers/ucb_rails/extractable_helper.rb
@@ -1,32 +1,6 @@
 # These methods should be extracted into own gem.
 module UcbRails::ExtractableHelper
   
-  def js_void
-    @_js_void ||= "javascript:void(0)"
-  end
-  
-  def boolean_display(boolean)
-    ( boolean ? '&#10004;' : '&nbsp;' ).html_safe
-  end
-
-  def th_actions(colspan=1)
-    content_tag(:th, 'Actions', colspan: colspan, class: 'c')
-  end
-  
-  def datetime_to_s(datetime, format)
-    if datetime.present?
-      datetime.localtime.to_s(format)
-    else
-      ''
-    end
-  end
-
-  def form_actions
-    content_tag(:div, class: 'form-actions') do
-      yield
-    end
-  end
-  
   def ucbr_table_tag(*args)
     options = canonicalize_options(args.extract_options!)
     
--- app/helpers/ucb_rails/lps_typeahead_helper.rb
+++ app/helpers/ucb_rails/lps_typeahead_helper.rb
@@ -2,6 +2,7 @@ module UcbRails::LpsTypeaheadHelper
   
   def lps_typeahead_search_field(options={})
     name = options.delete(:name) || 'person_search'
+    required = options.delete(:required)
     label = options.delete(:label) || 'User'
     value = options.delete(:value) || params[name]
     placeholder = options.delete(:placeholder) || 'Type name to search'
@@ -14,16 +15,36 @@ module UcbRails::LpsTypeaheadHelper
     search_url = options.delete(:search_url) || typeahead_search_ucb_rails_admin_users_path
     
     content_tag(:div, class: 'control-group lps-typeahead') do
-      label_tag(name, label) +
+      label_tag(name, class: label_classes(required)) do
+        required_marker(required) +
+        label
+         
+      end +
       content_tag(:div, class: 'controls') do
         content_tag(:div, class: 'input-append') do
-          text_field_tag(name, value, placeholder: placeholder, class: 'lps-typeahead-search', data: { uid_dom_id: uid_dom_id, url: search_url }) +
+          text_field_tag(name, value, placeholder: placeholder, class: 'typeahead-lps-search', data: { uid_dom_id: uid_dom_id, url: search_url }) +
           content_tag(:span, class: 'add-on') do
             icon('search', class: 'ldap-person-search', data: {search_field_name: name, result_link_text: result_link_text, result_link_class: result_link_class})
           end
         end +
         content_tag(:p, hint, class: 'help-block')
       end
+    end
+  end
+  
+  private
+  
+  def label_classes(required)
+    Array['control-label'].tap do |klasses|
+      klasses << 'required' if required
+    end
+  end
+  
+  def required_marker(required)
+    if required
+      content_tag(:abbr, '*', title: 'required') + ' '
+    else
+      ''
     end
   end
 end
