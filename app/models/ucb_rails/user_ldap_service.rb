4cf5f0659bee86a7f6e78715dd8eb3c3f7ed5839
Steve Downey <steve.downtown@gmail.com>
2013-06-18 18:00:42 -0700

    user ldap service

--- app/models/ucb_rails/user_ldap_service.rb
+++ app/models/ucb_rails/user_ldap_service.rb
@@ -3,11 +3,15 @@ class UcbRails::UserLdapService
   class << self
     
     def create_user_from_uid(uid)
+      UcbRails.logger.debug "create_user_from_uid #{uid}"
+      
       ldap_entry = UcbRails::LdapPerson::Finder.find_by_uid!(uid)
       create_user_from_ldap_entry(ldap_entry)
     end
     
     def create_user_from_ldap_entry(ldap_entry)
+      UcbRails.logger.debug "create_user_from_ldap_entry #{ldap_entry.uid}"
+      
       UcbRails::User.create! do |u|
         u.uid = ldap_entry.uid
         u.first_name = ldap_entry.first_name
@@ -18,11 +22,15 @@ class UcbRails::UserLdapService
     end
 
     def update_user_from_uid(uid)
+      UcbRails.logger.debug "update_user_from_uid #{uid}"
+      
       ldap_entry = UcbRails::LdapPerson::Finder.find_by_uid!(uid)
       update_user_from_ldap_entry(ldap_entry)
     end
 
     def update_user_from_ldap_entry(ldap_entry)
+      UcbRails.logger.debug "update_user_from_ldap_entry #{ldap_entry.uid}"
+      
       UcbRails::User.find_by_uid!(ldap_entry.uid).tap do |user|
         user.first_name = ldap_entry.first_name
         user.last_name = ldap_entry.last_name
